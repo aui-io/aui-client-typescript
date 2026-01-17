2d70e913ece9d1c16aba8f8f6fcfdc0713db7d1b
alakmashaqi <alaam@aui.io>
2026-01-18 01:00:20 +0200

    fix: Export ApolloWsSessionSocket type for TypeScript users
    Added direct export of ApolloWsSessionSocket so users can import it as:
    import { ApolloWsSessionSocket } from '@aui.io/aui-client'
    
    Changes:
    - src/exports.ts: Added ApolloWsSessionSocket export (safe custom exports file)
    - src/api/.../client/index.ts: Export Socket class from internal chain
    - .fernignore: Added exports.ts (index.ts NOT added - stays regeneratable)
    
    This is a safe fix that won't break future Fern generations.

--- src/api/resources/apolloWsSession/client/index.ts
+++ src/api/resources/apolloWsSession/client/index.ts
@@ -1,1 +1,1 @@
-export {};
+export { ApolloWsSessionSocket } from "./Socket.js";
--- src/exports.ts
+++ src/exports.ts
@@ -1,1 +1,10 @@
-export * from "./core/exports.js";
+/**
+ * Custom exports for the Apollo SDK
+ * 
+ * This file is in .fernignore and won't be overwritten by Fern generation.
+ * Add any custom exports here that need to be available at the package root.
+ */
+
+// Direct export of WebSocket Socket class for TypeScript users
+// This allows: import { ApolloWsSessionSocket } from '@aui.io/aui-client'
+export { ApolloWsSessionSocket } from "./api/resources/apolloWsSession/client/Socket.js";
